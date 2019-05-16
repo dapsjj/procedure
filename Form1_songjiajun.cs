ada553a63103d7d5a21828507ddd0260dfca4262
Rhaegal <dapsjj@qq.com>
2019-05-16 15:44:03 +0800

    Update Form1_songjiajun.cs

--- Form1_songjiajun.cs
+++ Form1_songjiajun.cs
@@ -55,8 +55,8 @@ namespace TcclMail
 					string systemName = "NewTimeCard_EntranceSendEmail";
 					//string appKey = "1e85bfb42d296646db1a026da6950d02164f3444c58ed3b0a60c8e9dfe0c4eedce17f6e2b975187f5623bf72428f1da7be97f56c7ddfc6cfc3b819db9ac72a33";//dingmingguang
 					string appKey = "664b54dfb19c87926c56dd086ba68a7a15ca11511c252eb57fff22cc3f7d65993d4aed427bdabafa550a7028c04e4a912de6c34fe972e4d9130bfdaf22a968f3"//use for test
-					//receivers = myDataSet.Tables[j].Rows[0]["EmployeeCD"].ToString();//一番目の上司を獲得する
-					receivers = "E10113982";
+					//receivers = "E" + myDataSet.Tables[j].Rows[0]["EmployeeCD"].ToString();//一番目の上司を獲得する
+					receivers = "E"+"10113982";
 					//string employeeCode = myDataSet.Tables[j].Rows[i]["EmployeeCode"].ToString();
 					//string employeeName = myDataSet.Tables[j].Rows[i]["EmployeeName"].ToString();
 					string OverTimeMessage = myDataSet.Tables[j].Rows[0]["OverTimeMessage"].ToString();//一番目のOverTimeMessageを獲得する
