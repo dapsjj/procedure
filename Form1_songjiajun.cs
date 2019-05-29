a4213ee2a6c37ef5f62a29259c0c6497f8813e11
Rhaegal <dapsjj@qq.com>
2019-05-29 09:18:05 +0800

    Update Form1_songjiajun.cs

--- Form1_songjiajun.cs
+++ Form1_songjiajun.cs
@@ -47,8 +47,8 @@ namespace TcclMail
 					string blockID = "0";
 					string topicTypeID = "3";//作業指示
 					//string topicCreaterCD = "99000109";
-					//string topicCreaterCD = "10113982";
-                    string topicCreaterCD = "10123238";//三井
+					string topicCreaterCD = "10113982";//songjiajun 
+                    //string topicCreaterCD = "90001058";//
                     string topicTitle = "残業時間調整指示";
 					string topicContent = "";
 					string fileIDs = "";
@@ -64,8 +64,11 @@ namespace TcclMail
 					//string systemName = "AsEmployeeUpdate";//dingmingguang
                     //string appKey = "1e85bfb42d296646db1a026da6950d02164f3444c58ed3b0a60c8e9dfe0c4eedce17f6e2b975187f5623bf72428f1da7be97f56c7ddfc6cfc3b819db9ac72a33";//dingmingguang
 
-                    string systemName = "overtimecheck";
-                    string appKey = "664b54dfb19c87926c56dd086ba68a7a15ca11511c252eb57fff22cc3f7d65993d4aed427bdabafa550a7028c04e4a912de6c34fe972e4d9130bfdaf22a968f3";//use for test
+                    //string systemName = "overtimecheck";
+                    //string appKey = "664b54dfb19c87926c56dd086ba68a7a15ca11511c252eb57fff22cc3f7d65993d4aed427bdabafa550a7028c04e4a912de6c34fe972e4d9130bfdaf22a968f3";//use for test
+
+                    string systemName = "NewTimeCard_Entrance";
+                    string appKey = "af51bc9cea7c384850c007ada22d54cd3a680b607b019d7bc1d9901d01017dec727c9faa14d1c256a715d2981686395f1d57f623c592b526895558fa12785820";
 
                     //string systemName = "Web_OsechiCakeOrder";//cuiweixia
                     //string appKey = "6869a849a9dd3f6e84a7c06c1eaf7c7803acd1bd34f7a912a37077d172406371d6001eddcb15b89b5594742f29449ca2b543508a6d46458271dca687c34a3001";//cuiweixia
@@ -108,14 +111,14 @@ namespace TcclMail
 					
 					if (myDataSet.Tables[j].Rows.Count > 0){//データがあればメールします。
 
-                        DateTime beforDT = System.DateTime.Now;
-                        string str_mail = TC.AddOtherSystemTopicInfo(blockID, topicTypeID, topicCreaterCD, topicTitle, topicContent, fileIDs, receivers, doEndTime, importanceID, isPush, isOnlyReceivers, tagID, _tagNM, systemName, appKey);
-                        DateTime afterDT = System.DateTime.Now;
-                        TimeSpan ts = afterDT.Subtract(beforDT);
-                        string myTime = ts.ToString();
-                        WriteLog(myTime);
-                        WriteLog(receivers.ToString());
 
+                            DateTime beforDT = System.DateTime.Now;
+                            string str_mail = TC.AddOtherSystemTopicInfo(blockID, topicTypeID, topicCreaterCD, topicTitle, topicContent, fileIDs, receivers, doEndTime, importanceID, isPush, isOnlyReceivers, tagID, _tagNM, systemName, appKey);
+                            DateTime afterDT = System.DateTime.Now;
+                            TimeSpan ts = afterDT.Subtract(beforDT);
+                            string myTime = ts.ToString();
+                            WriteLog(myTime);
+                            WriteLog(receivers.ToString());
 
 
 						
@@ -169,8 +172,8 @@ namespace TcclMail
             {
                 using (StreamWriter sw = File.AppendText(logPath))
                 {
-                    sw.WriteLine("异常：" + e.Message);
-                    sw.WriteLine("时间：" + DateTime.Now.ToString("yyy-MM-dd HH:mm:ss"));
+                    sw.WriteLine("Exception：" + e.Message);
+                    sw.WriteLine("Time：" + DateTime.Now.ToString("yyy-MM-dd HH:mm:ss"));
                     sw.WriteLine("**************************************************");
                     sw.WriteLine();
                     sw.Flush();
